145790aecc84f2fd1709089372b924aa635c997b
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-02 17:46:09 -0500

    Update removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsToRemove) {
+    let outputArray = inputArray;
 
+    for (const itemToRemove of itemsToRemove) {
+        outputArray = outputArray.filter( function(item) {
+            return item !== itemToRemove;
+        });
+    }
+
+    return outputArray;
 };
 
 // Do not edit below this line
