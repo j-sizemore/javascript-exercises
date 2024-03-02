f66c198b8c9d245eeafd72d9e51605a6afceadba
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-02 17:22:04 -0500

    Code repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inputString, numOfRepetitions) {
+    let outputString = '';
 
+    if (
+        typeof inputString != 'string'
+        || typeof numOfRepetitions != 'number'
+        || numOfRepetitions < 0
+    ) return 'ERROR';
+
+    for (let i = 0; i < numOfRepetitions; i++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
