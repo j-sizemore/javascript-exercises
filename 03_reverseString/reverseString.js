97c94fa2bd8ac6e51e056ecc49aa783c78922fdf
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-02 17:27:54 -0500

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = '';
+    for (let i = inputString.length; i > 0; i--) {
+        outputString += inputString.charAt(i - 1);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
