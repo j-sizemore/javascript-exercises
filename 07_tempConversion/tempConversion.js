4f525561dfb3a8cd0b1c7f28455a661664db6fdf
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2024-03-02 18:16:38 -0500

    Update convertToFarenheit and convertToCelsius functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+    // (deg C) = ((deg F) - 32) * 5 / 9
+    // multiply and divide by 10 to round to one decimal place
+    return Math.round((tempInFarenheit - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  // (deg F) = (deg C) * 9 / 5 + 32
+  // multiply and divide by 10 to round to one decimal place
+  return Math.round((tempInCelsius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
