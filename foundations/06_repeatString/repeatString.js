a6329ae360dcbaf831348565050373d866062e7c
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-06 12:09:09 -0400

    Complete exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(message, count) {
+    if (count < 0) return 'ERROR';
 
+    let result = '';
+
+    for (let i = 0; i < count; i++) {
+        result += message;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
