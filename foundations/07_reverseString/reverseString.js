34380df22a29b343f9c75a4790630d70925629e1
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-06 12:14:49 -0400

    Complete exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(message) {
+    let result = '';
+    let len = message.length;
 
+    for (let i = 0; i < len; i++) {
+        result = message.at(i) + result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
