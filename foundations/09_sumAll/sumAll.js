9cbeef7f709b0b1d3cd761487cdfb345db6b59f0
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-06 12:38:26 -0400

    Complete exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(boundA, boundB) {
+    // input validation
+    if (
+        !Number.isInteger(boundA)
+        || !Number.isInteger(boundB)
+        || boundA < 0
+        || boundB < 0
+    ) return 'ERROR';
 
+    let lo = Math.min(boundA, boundB);
+    let hi = Math.max(boundA, boundB);
+    let sum = 0;
+
+    for (let i = lo; i <= hi; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
