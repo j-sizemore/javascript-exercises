bc246a652b545ca2b84d4dae97dd4d076313dfe4
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-06 12:51:19 -0400

    Complete exerice 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  // tempC = (tempF - 32) * 5/9
+  let scale = 5 / 9;
+  let shift = 32;
+
+  let result = (tempF - shift) * scale;
+
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  // tempF = (tempC x 9/5 + 32)
+  let scale = 9 / 5;
+  let shift = 32;
+
+  let result = (tempC * scale) + shift;
+
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
