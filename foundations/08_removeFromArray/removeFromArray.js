6bd8040bc43acec1a092a2c359174b06a028a468
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-06 12:27:02 -0400

    Complete exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter((elem) => {
+        for (const arg of toRemove) {
+            if (elem === arg) return false;
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
