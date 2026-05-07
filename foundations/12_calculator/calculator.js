8beb849c9051f7fb5fe25438f1c8676198ed74cd
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-07 12:35:58 -0400

    Complete exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, elem) => {return sum + elem}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((prod, elem) => {return prod * elem}, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	if (exp == 0) return base;
+
+  let prod = 1;
+
+  for (let i = 0; i < exp; i++) {
+    prod *= base;
+  }
+
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+
+  let prod = 1;
+
+  for (let i = num; i > 1; i--) {
+    prod *= i;
+  }
+
+  return prod;
 };
 
 // Do not edit below this line
