cea4ccfa8b8a5bee1c153c7544f6bee0332a49b5
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-07 14:28:39 -0400

    Complete exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArray) {
 
+    let thisYear = new Date().getFullYear();
+
+    let ageArray = personArray.map((person) => (
+        (person.yearOfDeath || thisYear) - person.yearOfBirth
+    ));
+
+    let maxAgeIndex = ageArray.reduce((maxAgeIdx, currAge, currIdx, arr) => (
+        currAge > arr[maxAgeIdx] ? currIdx : maxAgeIdx
+    ), 0);
+
+    return personArray[maxAgeIndex];
+    
 };
 
 // Do not edit below this line
