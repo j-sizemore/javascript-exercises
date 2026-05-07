7495686fd010705bb1e5d03b5a4a52d77d6bf35d
Jacquelyn Sizemore <jacquelyn.sizemore@gmail.com>
2026-05-07 14:00:38 -0400

    Complete exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(inputIndex) {
 
+    let index = parseInt(inputIndex);
+
+    if (isNaN(index) || index < 0)
+        return "OOPS";
+
+    if (index == 0)
+        return 0;
+
+    let previous = [0, 0];
+    let current = 1;
+
+    for (let i = 2; i <= index; i++) {
+        previous[0] = previous[1];
+        previous[1] = current;
+        current = previous[0] + previous[1];
+    }
+
+    return current;
 };
 
 // Do not edit below this line
